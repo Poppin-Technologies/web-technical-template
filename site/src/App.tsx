107858bf47f9c0454bb1c2ee56296c77694f436f
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2024-07-22 11:22:22 -0700

    create template

---
+++ site/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-center"> Hello world! </h1>
+        </div>
+    );
+}
+
+export default App;
